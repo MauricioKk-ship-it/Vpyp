3f15b9eeb0a80ac7729d15095801abe4fd8dd9c2
Mauricio-100 <ceoseshell@gmail.com>
2025-10-07 00:00:03 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gospot-cli",
+    version="1.0.3",
+    description="GoSpot CLI - wrapper Python launching powerful shell tools for iSH/Linux",
+    author="Mauricio",
+    python_requires=">=3.8",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "gospot_pkg": ["*.sh", "sdk/*.sh"],
+    },
+    entry_points={
+        "console_scripts": [
+            "GoS = gospot_pkg.cli:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
